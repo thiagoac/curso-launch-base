603a88c0f0db40786b1e741b9c5c1abd00ca8270
Thiago Augusto <thiago.btw@gmail.com>
2020-01-29 17:01:03 -0300

    Operadores Lógico e condição

--- works/aulas/index.js
+++ works/aulas/index.js
@@ -1,1 +1,22 @@
-const nome = 'Thiago'
+// exercício calcula media
+
+const aluno01 = 'Thiago'
+const notaAluno01 = 9.8
+
+const aluno02 = 'Augusto'
+const notaAluno02 = 8
+
+const aluno03 = 'Costa'
+const notaAluno03 = 2
+
+const media = (notaAluno01+notaAluno02+notaAluno03)/3
+
+console.log(media)
+
+if(media >= 6){
+    console.log('Passou')
+}else{
+    console.log('reprovou')
+}
+
+
---
+++ works/aulas/operadores.js
@@ -0,0 +1,14 @@
+/*
+> maior
+< menor
+>= maior igual
+<= meno igual
+== igual a
+=== igual e do mesmo tipo
+=! diferente de
+==! diferente de, inclusive do tipo 
+*/
+
+console.log(5!=4)
+console.log(5>4)
+console.log(5<4)
